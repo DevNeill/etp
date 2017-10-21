6aa9840d2ed1e04014c9339fb3bf11408ec4851d
Devon Neill <devon.neill@gmail.com>
2017-10-21 15:42:57 +0200

    Foundation Done

--- app/controllers/tips_controller.rb
+++ app/controllers/tips_controller.rb
@@ -1,8 +1,34 @@
 class TipsController < ApplicationController
+  def index
+    @tips = Tip.all
+  end
+
+  def show
+    @tip = Tip.find(params[:id])
+  end
+
   def new
   end
 
-  def create
+  def edit
   end
-  
+
+  def create
+    @tip = Tip.new(tip_params)
+
+    @tip.save
+    redirect_to @tip
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def tip_params
+    params.require(:tip).permit(:title, :text)
+  end
+
 end
